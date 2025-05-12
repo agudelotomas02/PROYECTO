9873bf4d10b372de89cee475133b6e738d10c0a8
agudelotomas02 <agudelotomas02@gmail.com>
2025-05-12 07:12:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 const authRoutes = require('./routes/authRoutes');
